45b33fbb6c52de99447bc59b4fe8b10ae0b54157
Robby <singh.robbys13@gmail.com>
2025-04-04 20:31:06 -0500

    Stack implementation using a Singly Linked List

--- stackLinkedList.py
+++ stackLinkedList.py
@@ -7,19 +7,60 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.tail = None
 
     def push(self, value):
-        return
+        temp = Node(value)
+
+        if self.head is None:
+            self.head = temp
+        else:
+            curr_head = self.head
+            self.head = temp
+            temp.next = curr_head
 
     def pop(self):
-        return
+        if self.head is None:
+            print("List is empty!")
+            return -1
+        else:
+            curr_head = self.head
+            if curr_head.next is None:
+                self.head = None
+            else:
+                new_head = curr_head.next
+                self.head = new_head
+            return curr_head
 
     def peek(self):
-        return
+        if self.head is None:
+            print("List is empty!")
+            return -1
+        else:
+            return self.head.data
 
     def is_empty(self):
-        return
+        return self.head is None
 
     def size(self):
-        return
+        temp = self.head
+        count = 0
+
+        while temp:
+            count += 1
+            temp = temp.next
+        return count
+
+    def print_list(self):
+        temp = self.head
+
+        if temp is None:
+            print("List is empty!")
+
+        elif temp.next is None:
+            print(temp.data)
+
+        else:
+            while temp:
+                print(temp.data, end=" -> ")
+                temp = temp.next
+            print("None")
