9bf5cc5f3cbcdaa8afcc4cc4366aaf301fcaed1f
Robby <singh.robbys13@gmail.com>
2025-04-05 17:32:44 -0500

    Linked List based Queue implementation from scratch

--- queue/queueLinkedList.py
+++ queue/queueLinkedList.py
@@ -8,21 +8,54 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.count = 0
 
-    def is_Empty(self):
-        return
+    def is_empty(self):
+        return self.head is None
 
     def front(self):
-        return
+        return self.head.data
 
     def rear(self):
-        return
+        return self.tail.data
 
     def size(self):
-        return
+        return self.count
 
     def enqueue(self, element):
-        return
+        temp = Node(element)
+
+        if self.is_empty():
+            self.head = self.tail = temp
+
+        else:
+            curr_tail = self.tail
+            curr_tail.next = temp
+            self.tail = temp
+
+        self.count += 1
 
     def dequeue(self):
-        return
+        if self.is_empty():
+            print("Cannot dequeue from an empty queue!")
+            return -1
+        else:
+            data = self.head.data
+            self.head = self.head.next
+            if self.head is None:
+                self.tail = None
+
+        self.count -= 1
+        return data
+
+    def print_queue(self):
+        if self.is_empty():
+            print("Empty queue!")
+            return -1
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, end=" -> ")
+                temp = temp.next
+            print("None")
+
