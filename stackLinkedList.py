c920234b2ea52b0b4dc089e9b53e531944577533
Robby <singh.robbys13@gmail.com>
2025-04-04 20:01:35 -0500

    Skeleton code implementation of a Stack via Singly Linked List

---
+++ stackLinkedList.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def push(self, value):
+        return
+
+    def pop(self):
+        return
+
+    def peek(self):
+        return
+
+    def is_empty(self):
+        return
+
+    def size(self):
+        return
