8a81fc1a9d8af482546b4878be2428587abf301d
Robby <singh.robbys13@gmail.com>
2025-04-05 16:53:57 -0500

    Linked List based Queue implementation skeleton code

---
+++ queue/queueLinkedList.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def is_Empty(self):
+        return
+
+    def front(self):
+        return
+
+    def rear(self):
+        return
+
+    def size(self):
+        return
+
+    def enqueue(self, element):
+        return
+
+    def dequeue(self):
+        return
