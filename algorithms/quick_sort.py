7c1e5275240f8fd82c8f47407da633e51ac13035
Robby <singh.robbys13@gmail.com>
2025-04-11 19:53:17 -0500

    Quick sort implementation

---
+++ algorithms/quick_sort.py
@@ -0,0 +1,25 @@
+def quick_sort(arr):
+    def partition(low, high):
+        pivot = arr[high]
+        i = low - 1
+
+        for j in range(low, high):
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        return i + 1
+
+    def quick_sort_recursive(low, high):
+        if low < high:
+            pi = partition(low, high)
+            quick_sort_recursive(low, pi - 1)
+            quick_sort_recursive(pi + 1, high)
+
+    quick_sort_recursive(0, len(arr) - 1)
+    return arr
+
+
+test = [0, 9, 3, 8, 2, 7, 5]
+print(quick_sort(test))
