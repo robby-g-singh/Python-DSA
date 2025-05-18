21b97d8617b9d0c3769576dcf8142ec9356f46aa
Robby <singh.robbys13@gmail.com>
2025-05-17 22:03:32 -0500

    updated code and added BFS implementation

--- graphs/adjacency_list.py
+++ graphs/adjacency_list.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from collections import deque
 
 
 class Graph:
@@ -10,10 +11,35 @@ class Graph:
     def add_edge(self, u, v):  # u, v variables are similar to i,j but for graph theory
         if v not in self.adj[u]:  # checking for duplicate edges
             self.adj[u].append(v)  # Add node v to the adjacency list of node u, i.e. there is an edge from u to v
-        if not self.directed:  # if the graph is undirected (bi-directional),
-            if u not in self.adj[u]:
-                self.adj[v].append(u)  # add node u to the adjacency list of node v, i.e. there is an edge from v to u
+        if not self.directed and u not in self.adj[v]:  # if the graph is undirected (bi-directional)
+            self.adj[v].append(u)  # add node u to the adjacency list of node v, i.e. there is an edge from v to u
+
+    def bfs(self, start):
+        discovered = set()  # set to keep track of nodes already visited
+        queue = deque([start])  # initialize queue with starting node
+        discovered.add(start)  # same for discovered set
+
+        traversal = []  # empty list to preserve traversal order
+
+        while queue:  # loop through queue
+            current = queue.popleft()  # keep track of current node
+            traversal.append(current)  # update the traversal list
+
+            for neighbor in self.adj[current]:  # loop for the adjacent list for current
+                if neighbor not in discovered:  # if neighbor has not been visisited: add to discovered & queue
+                    discovered.add(neighbor)
+                    queue.append(neighbor)
+
+        return traversal  # return the order traversed!
 
     def print_graph(self):
         for node in self.adj:
             print(f"{node}: {self.adj[node]}")
+
+
+graph = Graph()
+graph.add_edge(1, 2)
+graph.add_edge(1, 3)
+graph.add_edge(2, 5)
+graph.add_edge(3, 4)
+print(graph.bfs())
